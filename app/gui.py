3e8f2c47990f38a38c19f0dd7e8fd01cf235ba6d
Svetlov-al <s3t1qs@gmail.com>
2023-07-07 19:44:05 +0400

    Implement add function

--- app/gui.py
+++ app/gui.py
@@ -3,14 +3,29 @@ import PySimpleGUI as sg
 
 
 label = sg.Text("Write a to-do")
-input_box = sg.InputText(tooltip="Enter to-do")
+input_box = sg.InputText(tooltip="Enter to-do", key='todo')
 add_button = sg.Button("Add")
 
 
-window = sg.Window("StepByStep: Goal and Task Organizer", layout=[[label], [input_box, add_button]])
-window.read()
-window.close()
+window = sg.Window("StepByStep: Goal and Task Organizer",
+                   layout=[[label], [input_box, add_button]],
+                   font=("Helvetica", 20))
 
+while True:
+    event, values = window.read()
+
+    match event:
+
+        case 'Add':
+            todos = functions.get_todos()
+            new_todo = values['todo'].capitalize() + '\n'
+            todos.append(new_todo)
+            print(new_todo)
+            functions.write_todos(todos)
+        case sg.WINDOW_CLOSED:
+            break
+
+window.close()
 
 
 
