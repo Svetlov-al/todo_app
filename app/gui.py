4307c3dec839d2200cfb23ebca90c8a0b9f5c4e9
Svetlov-al <s3t1qs@gmail.com>
2023-07-07 20:36:55 +0400

    Implement edit function

--- app/gui.py
+++ app/gui.py
@@ -5,15 +5,20 @@ import PySimpleGUI as sg
 label = sg.Text("Write a to-do")
 input_box = sg.InputText(tooltip="Enter to-do", key='todo')
 add_button = sg.Button("Add")
-
+list_box = sg.Listbox(values=functions.get_todos(), key='todos',
+                      enable_events=True,
+                      size=(35, 10))
+edit_button = sg.Button("Edit")
 
 window = sg.Window("StepByStep: Goal and Task Organizer",
-                   layout=[[label], [input_box, add_button]],
+                   layout=[[label], [input_box, add_button],
+                           [list_box, edit_button]],
                    font=("Helvetica", 20))
 
 while True:
     event, values = window.read()
-
+    print(event)
+    print(values)
     match event:
 
         case 'Add':
@@ -22,6 +27,17 @@ while True:
             todos.append(new_todo)
             print(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case 'Edit':
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo'].capitalize() + '\n'
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
         case sg.WINDOW_CLOSED:
             break
 
