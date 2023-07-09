09f5b6c684ed7148c29acd2e0449027f33d288a6
Svetlov-al <s3t1qs@gmail.com>
2023-07-09 21:07:29 +0400

    add complete feature

--- app/web.py
+++ app/web.py
@@ -8,14 +8,20 @@ def add_todo():
     todos.append(to_do + '\n')
     functions.write_todos(todos)
 
+
 st.title('ToDo App')
 st.subheader('This is your ToDos')
 st.write('This app is to increase your productivity!')
 
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
 
 st.text_input(label=' ', placeholder='Add new todo...',
               on_change=add_todo, key='new_todo')
-
