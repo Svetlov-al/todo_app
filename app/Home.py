787a5f9494e482a0eb0bfad7664d5bb3c8100de5
Svetlov-al <s3t1qs@gmail.com>
2023-07-21 20:24:41 +0400

    add send_email feature

--- app/Home.py
+++ app/Home.py
@@ -2,6 +2,8 @@ import streamlit as st
 import functions
 todos = functions.get_todos()
 
+st.set_page_config(layout='wide')
+
 
 def add_todo():
     to_do = st.session_state['new_todo']
@@ -11,8 +13,11 @@ def add_todo():
 
 st.title('ToDo App')
 st.subheader('This is your ToDos')
-st.write('This app is to increase your productivity!')
+st.write('This app is to increase your <b>productivity</b>!',
+         unsafe_allow_html=True)
 
+st.text_input(label=' ', placeholder='Add new todo...',
+              on_change=add_todo, key='new_todo')
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -23,5 +28,3 @@ for index, todo in enumerate(todos):
         st.experimental_rerun()
 
 
-st.text_input(label=' ', placeholder='Add new todo...',
-              on_change=add_todo, key='new_todo')
